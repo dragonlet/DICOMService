fa2d0db8ca4f9b5d19863035a49ad4de9f5bac56
irrer <irrer@med.umich.edu>
2013-08-20 17:12:26 -0400

    freshen AMQP connection as required
    If the AMQP server was restarted after the DICOM service had started,
    then the AMQP connection would be stale.  This change establishes a new
    connection if the connection is stale or old.

--- src/main/java/edu/umro/dicom/service/MRCT.scala
+++ src/main/java/edu/umro/dicom/service/MRCT.scala
@@ -45,137 +45,124 @@ class MRCT {
 
     private val config = ServiceConfig.getInstance.getMRCT
     private val MRCTDictionary = new MRCTDictionary
+    private val channelLifetime: Long = 60 * 60 * 1000
 
-    private def getChannel:Option[Channel] = {
+    class Chan(val channel: Channel, val expirationTime: Long);
+
+    var chan = new Chan(null, System.currentTimeMillis - 1)
+
+    private def initChannel(force: Boolean): Unit = {
+        if (force || (chan == null) || (chan.expirationTime < System.currentTimeMillis)) {
+            Log.get.info("Constructing new channel.  Using AMQP broker at " + ServiceConfig.getInstance.getAmqpBrokerHost + ":" + ServiceConfig.getInstance.getAmqpBrokerPort)
             try {
                 val factory = new ConnectionFactory
                 factory.setHost(ServiceConfig.getInstance.getAmqpBrokerHost)
                 factory.setPort(ServiceConfig.getInstance.getAmqpBrokerPort)
                 val connection = factory.newConnection
-                connection.createChannel match {
-                    case channel:Channel => {
-                        Log.get.info("Using AMQP broker at " + ServiceConfig.getInstance.getAmqpBrokerHost + ":" + ServiceConfig.getInstance.getAmqpBrokerPort)
-                        Some(channel)
-                    }
-                    case _ => {
-                        Log.get.severe(
-                                "Unable to get AMQP channel, channel already in use. to AMQP broker at " +
-                                ServiceConfig.getInstance.getAmqpBrokerHost + ":" + ServiceConfig.getInstance.getAmqpBrokerPort)
-                                None
-                    }
-                }
-            }
-            catch {
-                case e:Exception => {
+                chan = new Chan(connection.createChannel, System.currentTimeMillis+channelLifetime)
+
+                try chan.channel.exchangeDeclare(config.dataExchange, "topic", false)
+                catch { case e: Exception => Log.get.warning("Unable to declare data exchange: " + config.dataExchange) }
+
+                try chan.channel.exchangeDeclare(config.dataExchange, "topic", false)
+                catch { case e: Exception => Log.get.warning("Unable to declare data exchange: " + config.dataExchange) }
+            } catch {
+                case e: Exception => {
                     Log.get.severe(
-                            "Unable to get AMQP channel.  Is the broker running?  AMQP broker at " +
+                        "Unable to get AMQP channel.  Is the broker running?  AMQP broker at " +
                             ServiceConfig.getInstance.getAmqpBrokerHost + ":" + ServiceConfig.getInstance.getAmqpBrokerPort +
                             " : " + e)
-                            None
                 }
             }
+        }
     }
+    
+    initChannel(true)
 
-    // val PROGRAM_NAME = "MRCT";
     private class RecvDicom extends Object with ReceiveDicom {
         private val prettyPrinter = new PrettyPrinter(1000, 2)
         private val timeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
         private val ipAddress = OpSys.getHostIPAddress()
         private val agentId = config.agentName + "-" + ipAddress + "-" + (new Date).toString.replace(' ', '-')
 
-        val channel = getChannel
-
-        try channel.get.exchangeDeclare(config.dataExchange, "topic", false)
-        catch {case e:Exception => Log.get.warning("Unable to declare data exchange: " + config.dataExchange)}
-
-        try channel.get.exchangeDeclare(config.dataExchange, "topic", false)
-        catch {case e:Exception => Log.get.warning("Unable to declare data exchange: " + config.dataExchange)}
-        
-        private def generateEventId:String =
+        private def generateEventId: String =
             (new UID()).toString.replaceAll("^[^0-9a-zA-Z]*", "").replaceAll("[^0-9a-zA-Z][^0-9a-zA-Z]*", "_");
 
-
-        private var destPacs:Option[PACS] = {
-                PACS.findPacs(ServiceConfig.getInstance.getMRCT.destinationPacs) match {
-                    case pacs:PACS => Some(pacs)
-                    case _ => None
-                }
+        private var destPacs: Option[PACS] = {
+            PACS.findPacs(ServiceConfig.getInstance.getMRCT.destinationPacs) match {
+                case pacs: PACS => Some(pacs)
+                case _          => None
+            }
         }
-        private def buildEvent(attributeList:AttributeList, sourceAETitle:String):String = {
-                def getValue(tag:AttributeTag):String =
+        private def buildEvent(attributeList: AttributeList, sourceAETitle: String): String = {
+                def getValue(tag: AttributeTag): String =
                     attributeList.get(tag) match {
-                        case a:Attribute => a.getSingleStringValueOrEmptyString
-                        case _ => ""
+                        case a: Attribute => a.getSingleStringValueOrEmptyString
+                        case _            => ""
+                    }
+
+            val special = "<&>\"'$".toList
+                def gotSpecial(text: String) = text.toList.intersect(special).length > 0
+
+                def get(tag: AttributeTag): Elem = {
+                    val text = getValue(tag)
+                    <Label>{ if (gotSpecial(text)) PCData(text) else text }</Label>.copy(label = MRCTDictionary.getNameFromTag(tag))
                 }
 
-                val special = "<&>\"'$".toList
-                def gotSpecial(text:String) = text.toList.intersect(special).length > 0
+            val dicomList = List(
+                TagFromName.PatientName,
+                TagFromName.PatientID,
+                TagFromName.PatientBirthDate,
+                TagFromName.PatientSex,
+                TagFromName.SOPInstanceUID,
+                TagFromName.SeriesInstanceUID,
+                TagFromName.StudyInstanceUID,
+                TagFromName.StudyDate,
+                TagFromName.StudyTime,
+                TagFromName.SeriesTime,
+                TagFromName.StudyDescription,
+                TagFromName.AcquisitionTime,
+                TagFromName.ContentTime,
+                TagFromName.SeriesDescription,
+                TagFromName.OperatorsName,
+                TagFromName.ImageType,
+                TagFromName.Modality,
+                TagFromName.MediaStorageSOPClassUID,
+                TagFromName.PatientComments,
+                TagFromName.PatientPosition,
+                TagFromName.SeriesNumber,
+                TagFromName.StudyID,
+                TagFromName.RequestedProcedureDescription,
+                TagFromName.PerformedProcedureStepStartDate,
+                TagFromName.PerformedProcedureStepStartTime,
+                TagFromName.PerformedProcedureStepID,
+                TagFromName.PerformedProcedureStepDescription,
+                TagFromName.CommentsOnThePerformedProcedureStep,
+                TagFromName.Manufacturer,
+                TagFromName.ManufacturerModelName,
+                TagFromName.SoftwareVersions,
+                TagFromName.EchoTime,
+                TagFromName.EchoNumbers,
+                TagFromName.LargestImagePixelValue,
+                TagFromName.WindowCenter,
+                TagFromName.WindowWidth)
 
-                def get(tag:AttributeTag):Elem = {
-                        val text = getValue(tag)
-                        <Label>{if (gotSpecial(text)) PCData(text) else text}</Label>.copy(label = MRCTDictionary.getNameFromTag(tag))
-                }
+            val xml = <EventDicomObject xmlns='urn:EventDicomObject'>
+                          <Fid>{ destPacs match { case pacs: PACS => pacs.aeTitle; case _ => "" } }</Fid>
+                          { dicomList.map(t => get(t)) }
+                          <Header>
+                              <AgentId>{ config.agentName + "-" + ipAddress + "-" + (new Date) }</AgentId>
+                              <AgentName>{ config.agentName }</AgentName>
+                              <EventId>{ generateEventId }</EventId>
+                              <EventDateTime>{ timeFormat.format(System.currentTimeMillis) }</EventDateTime>
+                              <EventSourceName>{ sourceAETitle }</EventSourceName>
+                              <InResponseTo></InResponseTo>
+                              <IpAddress>{ ipAddress }</IpAddress>
+                          </Header>
+                      </EventDicomObject>;
 
-                val dicomList = List(
-                        TagFromName.PatientName,
-                        TagFromName.PatientID,
-                        TagFromName.PatientBirthDate,
-                        TagFromName.PatientSex,
-                        TagFromName.SOPInstanceUID,
-                        TagFromName.SeriesInstanceUID,
-                        TagFromName.StudyInstanceUID,
-                        TagFromName.StudyDate,
-                        TagFromName.StudyTime,
-                        TagFromName.SeriesTime,
-                        TagFromName.StudyDescription,
-                        TagFromName.AcquisitionTime,
-                        TagFromName.ContentTime,
-                        TagFromName.SeriesDescription,
-                        TagFromName.OperatorsName,
-                        TagFromName.ImageType,
-                        TagFromName.Modality,
-                        TagFromName.MediaStorageSOPClassUID,
-                        TagFromName.PatientComments,
-                        TagFromName.PatientPosition,
-                        TagFromName.SeriesNumber,
-                        TagFromName.StudyID,
-                        TagFromName.RequestedProcedureDescription,
-                        TagFromName.PerformedProcedureStepStartDate,
-                        TagFromName.PerformedProcedureStepStartTime,
-                        TagFromName.PerformedProcedureStepID,
-                        TagFromName.PerformedProcedureStepDescription,
-                        TagFromName.CommentsOnThePerformedProcedureStep,
-                        TagFromName.Manufacturer,
-                        TagFromName.ManufacturerModelName,
-                        TagFromName.SoftwareVersions,
-                        TagFromName.EchoTime,
-                        TagFromName.EchoNumbers,
-                        TagFromName.LargestImagePixelValue,
-                        TagFromName.WindowCenter,
-                        TagFromName.WindowWidth
-                )
-
-
-                val xml = <EventDicomObject xmlns='urn:EventDicomObject'>
-                <Fid>{destPacs match { case pacs:PACS => pacs.aeTitle ; case _ => ""}}</Fid>
-
-                {dicomList.map(t => get(t))}
-
-                <Header>
-                <AgentId>{config.agentName + "-" + ipAddress + "-" + (new Date)}</AgentId>
-                <AgentName>{config.agentName}</AgentName>
-                <EventId>{generateEventId}</EventId>
-                <EventDateTime>{timeFormat.format(System.currentTimeMillis)}</EventDateTime>
-                <EventSourceName>{sourceAETitle}</EventSourceName>
-                <InResponseTo></InResponseTo>
-                <IpAddress>{ipAddress}</IpAddress>
-                </Header>
-
-                </EventDicomObject>
-                ;
-
-                val text = "<?xml version='1.0'?>\n" + prettyPrinter.format(xml)
-                text
+            val text = "<?xml version='1.0'?>\n" + prettyPrinter.format(xml)
+            text
         }
 
         // /* This code sends an attribute list to the PFI.  It was considered once but is now obsolete.  It is here because
@@ -198,72 +185,71 @@ class MRCT {
         //            client.putByteArray(outStream.toByteArray, url, false)
         //}
 
-
         /**
          * If a MRCT/AETitle has been specified in the configuration, then
          * send the DICOM to the given PACS.
          */
-        private def sendToPACS(attributeList:AttributeList):Unit = {
-                destPacs match {
-                    case pacs:Some[PACS] => {
-                        val dicomPush:DicomPush = new DicomPush(pacs.get, Array(attributeList))
+        private def sendToPACS(attributeList: AttributeList): Unit = {
+            destPacs match {
+                case pacs: Some[PACS] => {
+                    val dicomPush: DicomPush = new DicomPush(pacs.get, Array(attributeList))
                     val message = dicomPush.push
                     if (message == null) Log.get.info("Sent MR CT file to PACS " + destPacs)
                     else Log.get.warning("Unable to send file to PACS: " + message)
-                    }
-                    case _ => ;
                 }
+                case _ => ;
+            }
         }
 
+        private def sendToDicomToCoordinator(eventText: Array[Byte], attributeList: AttributeList): Unit = {
+            try {
+                initChannel(false)
+                val bout = new ByteArrayOutputStream(16 * 1024 * 1024);
+                var length = eventText.length.toString + " "
+                bout.write(length.getBytes)
+                bout.write(eventText)
 
-        private def sendToDicomToCoordinator(eventText:Array[Byte], attributeList:AttributeList):Unit = {
-                try {
-                    val bout = new ByteArrayOutputStream(16 * 1024 * 1024);
-                    var length = eventText.length.toString + " "
-                    bout.write(length.getBytes)
-                    bout.write(eventText)
-
-                    val dicomOutputStream = new DicomOutputStream(bout,
-                            TransferSyntax.ExplicitVRLittleEndian, TransferSyntax.ExplicitVRLittleEndian);
-                    attributeList.write(dicomOutputStream);
-                    dicomOutputStream.flush;
-                    Log.get.info("Sending AMQP message via broker to MRCT exchange: " + config.dataExchange + "   routing key: " +
-                            config.dataRoutingKey + "    header: " + new String(eventText))
-                    channel.get.basicPublish(config.dataExchange, config.dataRoutingKey, null, bout.toByteArray)
+                val dicomOutputStream = new DicomOutputStream(bout,
+                    TransferSyntax.ExplicitVRLittleEndian, TransferSyntax.ExplicitVRLittleEndian);
+                attributeList.write(dicomOutputStream);
+                dicomOutputStream.flush;
+                Log.get.info("Sending AMQP message via broker to MRCT exchange: " + config.dataExchange + "   routing key: " +
+                    config.dataRoutingKey + "    header: " + new String(eventText))
+                chan.channel.basicPublish(config.dataExchange, config.dataRoutingKey, null, bout.toByteArray)
+            } catch {
+                case e: Exception => {
+                    Log.get.warning("Unable to send DICOM file to MR CT Coordinator: " + e + "\nEvent: " + new String(eventText))
+                    Thread.sleep(10 * 1000)
+                    Log.get.info("Attempting AMQP channel reset ...")
+                    initChannel(true)
+                    sendToDicomToCoordinator(eventText, attributeList)
                 }
-                catch {
-                    case e:Exception => Log.get.warning("Unable to send DICOM file to MR CT Coordinator: " + e + "\nEvent: " + new String(eventText))
-                }
+            }
         }
 
-
-        override def receive(attributeList:AttributeList, transferSyntax:String, sourceAETitle:String) {
+        override def receive(attributeList: AttributeList, transferSyntax: String, sourceAETitle: String) {
             try {
                 val modality = attributeList.get(TagFromName.SOPClassUID).getSingleStringValueOrEmptyString
                 if (modality == SOPClass.KeyObjectSelectionDocumentStorage) {
                     Log.get.info("MR CT ignoring Key Object (KO) file...");
-                }
-                else {
+                } else {
                     sendToPACS(attributeList)
-                    val eventText:Array[Byte] = buildEvent(attributeList, sourceAETitle).getBytes                    
+                    val eventText: Array[Byte] = buildEvent(attributeList, sourceAETitle).getBytes
                     sendToDicomToCoordinator(eventText, attributeList)
                 }
-            }
-            catch {
-                case e:Exception => {
+            } catch {
+                case e: Exception => {
                     Log.get.severe("Failed to relay MR CT image.  Exception: " + e + "\nDICOM: " + buildEvent(attributeList, sourceAETitle))
                 }
             }
         }
     }
 
-
     // Only run if AMQP broker properly configured.
     if ((ServiceConfig.getInstance.getAmqpBrokerHost != null) && (ServiceConfig.getInstance.getAmqpBrokerPort > 0)) {
         val receiver = new DicomReceiver(config.aeTitle, config.port)
 
         receiver.receive = new RecvDicom
-    }
-    else Log.get.info("AMQP is not configured, so MRCT functionality is disabled")
+    } else Log.get.info("AMQP is not configured, so MRCT functionality is disabled")
 
 }
